d025a226b6e24269b061d47ce01eff7598e59c2c
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-10-12 14:39:21 -0700

    Update num_guess.py

--- num_guess.py
+++ num_guess.py
@@ -2,10 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 print("Enter the integer for the player to guess")
-
 num=int(input())
 print("Enter your guess")
 guess=int(input())
