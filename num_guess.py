320d7c80a28e5b1ffdfa01b4993c3eb6563bd06e
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-10-12 18:33:45 -0700

    Update num_guess.py

--- num_guess.py
+++ num_guess.py
@@ -2,20 +2,20 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-print("Enter the integer for the player to guess")
+print("Enter the integer for the player to guess.")
 num=int(input())
-print("Enter your guess")
+print("Enter your guess.")
 guess=int(input())
 counter=0
 while guess!=num:
  if num<guess:
-    print("Too high - try again")
+    print("Too high - try again:")
     counter+=1
     guess=int(input())
  elif num>guess:
-    print("Too low - try again")
+    print("Too low - try again:")
     guess=int(input())
     counter+=1
 if num==guess:
  counter+=1
- print("You guessed it in", counter, "tries")
+ print("You guessed it in", counter, "tries.")
